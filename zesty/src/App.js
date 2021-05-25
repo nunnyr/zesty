510bb205f9eb0a00f0eeca08ece8aaaf2daec4a1
Nunny R <nunnyreyes@gmail.com>
2021-05-25 16:34:44 -0400

    set a message in app component

--- zesty/src/App.js
+++ zesty/src/App.js
@@ -1,11 +1,31 @@
-import './App.css';
+//import './App.css';
+import React from "react"
 
-function App() {
+//function definition for App component
+
+//Component is a reusable piece of code that returns an app element
+//You can build class and functional components
+//functional components take props as an object and return an element
+//React Hooks only works with functional components
+//Lifecycle methods are a series of events that happen between the initialization
+//or creation or unmounting of a component.
+
+
+const App = () => {
+
+
+  const building = "TPL building"
   return (
-    <div className="App">
-      
+    <div className = "App">
+      <h1>Hello SoHo</h1>
+      <h2>What will we build</h2>
+      <p>{building}</p>
+
+
     </div>
-  );
+
+  )
+
 }
 
 export default App;
