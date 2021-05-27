d316d65e3b4e087b3aafcda875c9a41871c46d8d
Nunny R <nunnyreyes@gmail.com>
2021-05-27 17:16:30 -0400

    changing div

--- zesty/src/App.js
+++ zesty/src/App.js
@@ -22,9 +22,10 @@ const images = [cabbage, mango, fig, kiss, peach, avocado]
 
 const Loading = () => (
   <aside>
-    <div className="loading-bar"></div>
+    <div className="loading-bar">
       <label htmlFor="images-loaded">Loading all your favorite images...</label>
       <progress id="images-loaded" max="100" value="50"></progress>
+    </div>
   </aside>
 )
 
